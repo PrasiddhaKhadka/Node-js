494de627c45bc55295cb508685a857e45a3d76ce
PrasiddhaKhadka <prasiddhak77@gmail.com>
2025-11-05 11:31:21 +0545

    first commit

---
+++ globals.js
@@ -0,0 +1,17 @@
+// __dirname = Path to current directory
+// __filename = File name
+// require = Function to import modules
+// module = Current module object
+// exports = Module exports object
+// process = Node.js process object
+
+
+
+console.log(__dirname);
+// console.log(__filename);
+// console.log(require);
+// console.log(exports);
+
+setInterval(()=>{
+    console.log("Hello World");
+},1000)
